8de45822419606487b3129347ec056146865a757
shuja-r <shujarahamanishuja@gmail.com>
2026-09-18 19:00:25 +0530

    updated the version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="deb_cicd_prac_project",
-    version="0.0.1",
+    version="0.0.2",
     description="This contains the code in the ./src directory of the project",
     author="Shuja",
     packages=find_packages(where="./src"),
