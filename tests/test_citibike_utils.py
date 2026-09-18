6f7bef4384d6173756a8b26ab5ea80195bcf0a73
shuja-r <shujarahamanishuja@gmail.com>
2026-09-18 19:30:48 +0530

    revert the fail changes

--- tests/test_citibike_utils.py
+++ tests/test_citibike_utils.py
@@ -30,7 +30,3 @@ def test_get_trip_duration_mins(spark):
     # Assert that the differences are as expected
     assert results[0]["trip_duration_mins"] == 10
     assert results[1]["trip_duration_mins"] == 30
-
-
-def test_to_fail():
-    assert 1 == 2
